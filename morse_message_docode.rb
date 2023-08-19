279b5b063e1941399ccd945b246c567d15c9ba57
porag-m06 <porag.m06@gmail.com>
2023-08-20 00:37:10 +0600

    code improved and fixed for the output

--- morse_message_docode.rb
+++ morse_message_docode.rb
@@ -1,15 +1,43 @@
-def decode_char(morse_code)
-    MORSE_CODE[morse_code]
-end
+def morse_to_char(morse_char)
+    morse_to_english = {
+      '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+      '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+      '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+      '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+      '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+      '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+      '-.--' => 'Y', '--..' => 'Z'
+    }
   
-def decode_word(morse_code)
-    morse_code.split.map { |char| MORSE_CODE[char] }.join
-end
+    morse_to_english[morse_char]
+  end
   
-def decode(message)
-    message.split("   ").map do |word|
-    word.split.map { |char| MORSE_CODE[char] }.join
-    end.join(" ")
-end
-
-puts decode(" .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+  def morse_to_word(morse_word)
+    morse_chars = morse_word.split(' ')
+    english_word = ""
+  
+    morse_chars.each do |morse_char|
+      english_char = morse_to_char(morse_char)
+      english_word += english_char if english_char
+    end
+  
+    english_word
+  end
+  
+  def morse_to_sentence(morse_code)
+    morse_words = morse_code.split('   ')
+    english_sentence = ""
+  
+    morse_words.each do |morse_word|
+      english_word = morse_to_word(morse_word)
+      english_sentence += english_word + " "
+    end
+  
+    english_sentence.strip
+  end
+  
+  # decoding message
+  input_morse = " .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+  english_sentence = morse_to_sentence(input_morse)
+  puts english_sentence
+  
