f4ea0d7ca78b095954a214dc380c26868a150d0c
porag-m06 <porag.m06@gmail.com>
2023-08-20 00:50:52 +0600

    update

--- morse_message_docode.rb
+++ morse_message_docode.rb
@@ -1,43 +1,42 @@
 def morse_to_char(morse_char)
-    morse_to_english = {
-      '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
-      '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
-      '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
-      '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
-      '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-      '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
-      '-.--' => 'Y', '--..' => 'Z'
-    }
+  morse_to_english = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+    '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+    '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z'
+  }
   
-    morse_to_english[morse_char]
+  morse_to_english[morse_char]
+end
+  
+def morse_to_word(morse_word)
+  morse_chars = morse_word.split(' ')
+  english_word = ''
+  
+  morse_chars.each do |morse_char|
+    english_char = morse_to_char(morse_char)
+    english_word += english_char if english_char
   end
   
-  def morse_to_word(morse_word)
-    morse_chars = morse_word.split(' ')
-    english_word = ''
+  english_word
+end
   
-    morse_chars.each do |morse_char|
-      english_char = morse_to_char(morse_char)
-      english_word += english_char if english_char
-    end
+def morse_to_sentence(morse_code)
+  morse_words = morse_code.split('   ')
+  english_sentence = ''
   
-    english_word
+  morse_words.each do |morse_word|
+    english_word = morse_to_word(morse_word)
+    english_sentence += "#{english_word} "
   end
   
-  def morse_to_sentence(morse_code)
-    morse_words = morse_code.split('   ')
-    english_sentence = ''
+  english_sentence.strip
+end
   
-    morse_words.each do |morse_word|
-      english_word = morse_to_word(morse_word)
-      english_sentence += "#{english_word} "
-    end
-  
-    english_sentence.strip
-  end
-  
-  # Decoding message
-  input_morse = ' .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
-  english_sentence = morse_to_sentence(input_morse)
-  puts english_sentence
-  
+# Decoding message
+input_morse = ' .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+english_sentence = morse_to_sentence(input_morse)
+puts english_sentence
